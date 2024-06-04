928e0abfb69b447d62f18c5b3135d8085ef30f02
Anastasia Lyah <lyahanastasya@gmail.com>
2024-06-04 21:47:53 +0300

    Добавьте файлы проекта.

---
+++ PR5_SPZ_TaskLib/AbonentLib.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_SPZ_TaskLib
+{
+    public class Abonent
+    {
+        public string Number;
+        public string Name;
+        public string Tariff;
+        public double Balance;
+        public DateTime ActivationDate;
+        public bool HasInternet;
+        public bool HasDigitalTV;
+        public TimeSpan timeExistence
+        {
+            get
+            {
+                return GetTimeExistence();
+            }
+        }
+        public TimeSpan GetTimeExistence()
+        {
+            return DateTime.Now - ActivationDate;
+        }
+    }
+}
