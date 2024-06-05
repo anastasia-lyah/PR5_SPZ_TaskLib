eb332ca020320560a942af3f3bee5dba06c3d59c
Anastasia Lyah <lyahanastasya@gmail.com>
2024-06-05 12:56:27 +0300

    сom1

---
+++ Abonent_App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR5_SPZ_TaskLib;
+
+namespace Abonent_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Abonent[] arrAbonents;
+
+            Console.Write("Введiть кiлькiсть абонентів: ");
+            int cntAbonents = int.Parse(Console.ReadLine());
+            arrAbonents = new Abonent[cntAbonents];
+
+            for (int i = 0; i < cntAbonents; i++)
+            {
+                Console.WriteLine("Введiть номер телефону: "); string sNumber = Console.ReadLine();
+                Console.WriteLine("Введiть iм'я абонента: "); string sName = Console.ReadLine();
+                Console.WriteLine("Введiть тарифний план: "); string sTariff = Console.ReadLine();
+                Console.WriteLine("Введiть баланс рахунку: "); string sBalance = Console.ReadLine();
+                Console.Write("Введiть дату активацiї номера (рррр-мм-дд): ");
+                string sActivationDate = Console.ReadLine();
+                Console.Write("Чи пiдключено iнтернет ? (y - так, n - нi) : ");
+                ConsoleKeyInfo keyHasInternet = Console.ReadKey(); Console.WriteLine();
+                Console.Write("Чи пiдключено цифрове ТБ ? (y - так, n - нi) : ");
+                ConsoleKeyInfo keyHasDigitalTV = Console.ReadKey(); Console.WriteLine();
+
+                Console.WriteLine();
+
+                Abonent abonent = new Abonent();
+
+                Abonent OurAbonent = new Abonent();
+                OurAbonent.Number = sNumber;
+                OurAbonent.Name = sName;
+                OurAbonent.Tariff = sTariff;
+                OurAbonent.Balance = double.Parse(sBalance);
+                OurAbonent.ActivationDate = DateTime.Parse(sActivationDate);
+                OurAbonent.HasInternet = keyHasInternet.Key == ConsoleKey.Y ? true : false;
+                OurAbonent.HasDigitalTV = keyHasDigitalTV.Key == ConsoleKey.Y ? true : false;                
+
+                arrAbonents[i] = abonent;
+            }
+
+            foreach (Abonent a in arrAbonents)
+            {
+                Console.WriteLine();
+                Console.WriteLine("_______________________________________________");
+                Console.WriteLine("\t\tДанi про абонента під номером {0}", a.Number);
+                Console.WriteLine("_______________________________________________");                              
+                Console.WriteLine("Iм'я: " + a.Name);
+                Console.WriteLine("Тарифний план: " + a.Tariff);
+                Console.WriteLine("Баланс: " + a.Balance);
+                Console.WriteLine("Дата активацiї: " + a.ActivationDate);
+                Console.WriteLine(a.HasInternet ? "Пiдключено iнтернет" : "Iнтернет не пiдключено");
+                Console.WriteLine(a.HasDigitalTV ? "Пiдключено цифрове ТБ" : "Цифрове ТБ не пiдключено");
+                Console.WriteLine($"Час iснування номера: {a.timeExistence.Days} днiв");
+                Console.WriteLine();                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Abonent_App1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PR5_SPZ_TaskLib;
+
+namespace Abonent_App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Abonent[] arrAbonents;
+
+            Console.Write("Введiть кiлькiсть абонентiв: ");
+            int cntAbonents = int.Parse(Console.ReadLine());
+            arrAbonents = new Abonent[cntAbonents];
+
+            for (int i = 0; i < cntAbonents; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("_________________________________________");
+                Console.WriteLine($"\t\tАбонент {i+1}");
+                Console.WriteLine("_________________________________________");
+                Console.ResetColor();
+                Console.WriteLine("Введiть номер телефону: "); string sNumber = Console.ReadLine();
+                Console.WriteLine("Введiть iм'я абонента: "); string sName = Console.ReadLine();
+                Console.WriteLine("Введiть тарифний план: "); string sTariff = Console.ReadLine();
+                Console.WriteLine("Введiть баланс рахунку: "); string sBalance = Console.ReadLine();
+                Console.Write("Введiть дату активацiї номера (рррр-мм-дд): ");
+                string sActivationDate = Console.ReadLine();
+                Console.Write("Чи пiдключено iнтернет ? (y - так, n - нi) : ");
+                ConsoleKeyInfo keyHasInternet = Console.ReadKey(); Console.WriteLine();
+                Console.Write("Чи пiдключено цифрове ТБ ? (y - так, n - нi) : ");
+                ConsoleKeyInfo keyHasDigitalTV = Console.ReadKey(); Console.WriteLine();
+
+                Console.WriteLine();
+
+                Abonent abonent = new Abonent();
+               
+                abonent.Number = sNumber;
+                abonent.Name = sName;
+                abonent.Tariff = sTariff;
+                abonent.Balance = double.Parse(sBalance);
+                abonent.ActivationDate = DateTime.Parse(sActivationDate);
+                abonent.HasInternet = keyHasInternet.Key == ConsoleKey.Y ? true : false;
+                abonent.HasDigitalTV = keyHasDigitalTV.Key == ConsoleKey.Y ? true : false;
+
+                arrAbonents[i] = abonent;
+            }
+
+            foreach (Abonent a in arrAbonents)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine();
+                Console.WriteLine("_______________________________________________");
+                Console.WriteLine("\tДанi про абонента з номером {0}", a.Number);
+                Console.WriteLine("_______________________________________________");
+                Console.ResetColor();
+                Console.WriteLine("Iм'я: " + a.Name);
+                Console.WriteLine("Тарифний план: " + a.Tariff);
+                Console.WriteLine("Баланс: " + a.Balance);
+                Console.WriteLine("Дата активацiї: " + a.ActivationDate);
+                Console.WriteLine(a.HasInternet ? "Пiдключено iнтернет" : "Iнтернет не пiдключено");
+                Console.WriteLine(a.HasDigitalTV ? "Пiдключено цифрове ТБ" : "Цифрове ТБ не пiдключено");
+                Console.WriteLine($"Час iснування номера: {a.timeExistence.Days} днiв");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
